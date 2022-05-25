09bf6d1f3d9ee41d864520f8264c41ea99aa49d3
woopackerpat <p.taechakraichana@gmail.com>
2022-05-25 13:49:55 +0700

    update and delete comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,5 @@
 const { Comment } = require("../models");
+const createError = require("../utils/createError");
 
 exports.createComment = async (req, res, next) => {
   try {
@@ -19,13 +20,34 @@ exports.updateComment = async (req, res, next) => {
   try {
     const { title } = req.body;
     const { id, postId } = req.params;
+    const comment = await Comment.findOne({ id, postId });
+    if (!comment) {
+      createError("comment not found", 400);
+    }
 
+    if (comment.userId !== req.user.id) {
+      createError("you have no permission", 403);
+    }
+
+    comment.title = title;
+    await comment.save();
+    res.json({ comment });
   } catch (err) {
     next(err);
   }
 };
 exports.deleteComment = async (req, res, next) => {
   try {
+    const { id, postId } = req.params;
+    const comment = await Comment.findOne({ id, postId });
+    if (!comment) {
+      createError("comment not found", 400);
+    }
+    if (comment.userId !== req.user.id) {
+      createError("you have no permission", 403);
+    }
+    await comment.destroy();
+    res.status(204).json();
   } catch (err) {
     next(err);
   }
