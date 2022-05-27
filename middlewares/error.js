70682515c9dd42ee6ff1b669a0f178e986555cdf
woopackerpat <p.taechakraichana@gmail.com>
2022-05-27 11:33:27 +0700

    complted 5

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,21 @@
 module.exports = (err, req, res, next) => {
-  console.log(err)
+  console.log(err);
+
+  if (
+    err.name === "SequelizeValidationError" ||
+    err.name === "SequelizeUniqueConstraintError"
+  ) {
+    err.statusCode = 400;
+    err.message = err.errors[0].message;
+  }
+
+  if (err.name === "TokenExpiredError") {
+    err.statusCode = 401;
+  }
+
+  if (err.name === "TokenWebTokenError") {
+    err.statusCode = 401;
+  }
+
   res.status(err.statusCode || 500).json({ message: err.message });
 };
--- models/Comment.js
+++ models/Comment.js
@@ -1,43 +1,41 @@
 module.exports = (sequelize, DataTypes) => {
-    const Comment = sequelize.define(
-      "Comment",
-      {
-          title: {
-              type: DataTypes.STRING,
-              allowNull: false,
-              validate: {
-                  notEmpty: true
-              }
-          }
+  const Comment = sequelize.define(
+    "Comment",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "title must not empty",
+          },
+        },
       },
-      {
-        underscored: true,
-      }
-    );
+    },
+    {
+      underscored: true,
+    }
+  );
 
-    Comment.associate = (models) => {
-      Comment.belongsTo(models.Post, {
-        foreignKey: {
-          allowNull: false,
-          name: "postId",
-          
-        },
-        onUpdate: "RESTRICT",
-        onDelete: "RESTRICT",
-      });
+  Comment.associate = (models) => {
+    Comment.belongsTo(models.Post, {
+      foreignKey: {
+        allowNull: false,
+        name: "postId",
+      },
+      onUpdate: "RESTRICT",
+      onDelete: "RESTRICT",
+    });
 
-      Comment.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false,
-          name: "userId",
-          
-        },
-        onUpdate: "RESTRICT",
-        onDelete: "RESTRICT",
-      });
-    };
-
-    
-    return Comment;
+    Comment.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+        name: "userId",
+      },
+      onUpdate: "RESTRICT",
+      onDelete: "RESTRICT",
+    });
   };
-  
+
+  return Comment;
+};
